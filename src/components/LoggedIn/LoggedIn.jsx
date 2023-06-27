fa819659ff9de430e372f3bc12a6932d41daafa2
Ryan Beck <ryanbeck@Jasmines-MacBook-Pro.local>
2023-06-27 13:36:46 -0400

    community pages display their posts

--- src/components/LoggedIn/LoggedIn.jsx
+++ src/components/LoggedIn/LoggedIn.jsx
@@ -1,5 +1,6 @@
 export default function LoggedIn({ currentUser, handleLogout }) {
 
+    console.log(currentUser)
 
     return ( 
         <>
---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./PostCard.css"
+
+export default function HomePage({posts}) {
+  console.log(posts)
+  return (
+    <>
+    
+    <div className="postBox">
+      <h5>{posts.userId.username}</h5>
+      <h6>{posts.title}</h6>
+      <p>{posts.content}</p>
+    </div>
+    
+    <hr/>
+    </>
+    
+  )
+}
